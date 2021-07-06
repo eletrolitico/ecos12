521c0f5e98290af120b9a936b61514d7873de4f6
Henrique Possatto <henriquempossatto@gmail.com>
2021-07-06 17:29:57 -0300

    dev

--- Rede/Server.cpp
+++ Rede/Server.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 
+#define ASIO_STANDALONE
+#include <asio.hpp>
+#include <asio/ts/buffer.hpp>
+#include <asio/ts/internet.hpp>
+
 int main()
 {
+    asio::error_code ec;
+    asio::io_context context;
+
     std::cout << "Hello there" << std::endl;
     return 0;
 }
